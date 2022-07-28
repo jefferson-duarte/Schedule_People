7352e16bf334d43eb4dc62d4c6c651d726a7f5b0
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-28 11:49:32 +0100

    Add delete and search people

--- people/forms.py
+++ people/forms.py
@@ -1,8 +1,14 @@
-from django.forms import ModelForm
+from django.forms import DateField, ModelForm, TextInput
 from .models import People
 
 
 class PeopleForm(ModelForm):
+    birth_date = DateField(
+        widget=TextInput(
+            attrs={'type': 'date'}
+        )
+    )
+
     class Meta:
         model = People
         fields = ['full_name', 'birth_date', 'active']
--- people/urls.py
+++ people/urls.py
@@ -3,5 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.ListPeopleView.as_view(), name='list_people'),
-    path('create/', views.PeopleCreateView.as_view(), name='new_people')
+    path('create/', views.PeopleCreateView.as_view(), name='new_people'),
+    path(
+        'update/<int:pk>/',
+        views.PeopleUpdateView.as_view(),
+        name='update_people'
+    ),
+    path(
+        'delete/<int:pk>/',
+        views.PeopleDeleteView.as_view(),
+        name='delete_people'
+    ),
 ]
